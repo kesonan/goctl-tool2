a951fe32ed85ea775f460f40bc3e726987eadedd
kesonan <anqiansong@gmail.com>
2024-07-24 15:16:42 +0800

    relayout

--- web/src/components/generator/model/Mysql.tsx
+++ web/src/components/generator/model/Mysql.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import type { CheckboxProps } from "antd";
+import { CheckboxProps, Modal } from "antd";
 import {
   Button,
   Checkbox,
@@ -23,57 +23,102 @@ const Mysql: React.FC = () => {
   const [form] = Form.useForm();
   const [code, setCode] = useState("");
   const [passwordVisible, setPasswordVisible] = React.useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const onChange: CheckboxProps["onChange"] = (e) => {
     console.log(`checked = ${e.target.checked}`);
   };
+  const onFinish = (obj: any) => {
+    setModalOpen(true);
+  };
 
   return (
-    <Flex className="mysql" gap={1} wrap>
-      <Form form={form} layout="vertical" className={"mysql-form-container"}>
-        <Flex vertical wrap justify={"space-around"}>
-          <Divider orientation="left">{t("mysqlConnectionTitle")}</Divider>
-          <div className={"mysql-connection-panel"}>
-            <Flex flex={1} gap={10} wrap>
-              <Form.Item
-                label={t("mysqlHost")}
-                style={{ flex: 0.7 }}
-                name={"host"}
-                tooltip={t("mysqlHostTooltip")}
-              >
-                <Input
-                  placeholder={`${t("formInputPrefix")}${t("mysqlHost")}`}
-                />
-              </Form.Item>
-              <Form.Item
-                label={t("mysqlPort")}
-                style={{ flex: 0.3 }}
-                name={"port"}
-              >
-                <InputNumber
-                  style={{ width: "100%" }}
-                  placeholder={`${t("formInputPrefix")}${t("mysqlPort")}`}
-                  min={0}
-                  max={65535}
-                  precision={0}
-                />
-              </Form.Item>
-            </Flex>
+    <>
+      <Modal
+        title={t("mysqlModalTitle")}
+        style={{ top: 20 }}
+        open={modalOpen}
+        maskClosable={false}
+        closable={false}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
+        okText={t("mysqlModalOkTitle")}
+        footer={[
+          <Checkbox>{t("mysqlModalCheckTitle")}</Checkbox>,
+          <Button
+            key="submit"
+            type="primary"
+            onClick={() => {
+              setModalOpen(false);
+            }}
+          >
+            {t("mysqlModalOkTitle")}
+          </Button>,
+        ]}
+      >
+        <p>{t("mysqlModalContent")}</p>
+        <p>{t("mysqlModalContent1")}</p>
+        <p>{t("mysqlModalContent2")}</p>
+      </Modal>
 
-            <Flex vertical flex={1} wrap>
-              <Form.Item label={t("mysqlUsername")} name={"username"}>
-                <Input
-                  placeholder={`${t("formInputPrefix")}${t("mysqlUsername")}`}
-                />
-              </Form.Item>
-              <Form.Item label={t("mysqlPassword")} name={"password"}>
-                <Input.Password
-                  placeholder={`${t("formInputPrefix")}${t("mysqlPassword")}`}
-                  iconRender={(visible) =>
-                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-                  }
-                />
-              </Form.Item>
+      <Flex className="mysql" gap={1} wrap>
+        <Form
+          onFinish={onFinish}
+          form={form}
+          layout="vertical"
+          className={"mysql-form-container"}
+        >
+          <Flex vertical wrap justify={"space-around"}>
+            <Divider orientation="left">{t("mysqlConnectionTitle")}</Divider>
+            <div className={"mysql-connection-panel"}>
+              <Flex flex={1} gap={8} wrap>
+                <Form.Item
+                  label={t("mysqlHost")}
+                  style={{ flex: 0.7 }}
+                  name={"host"}
+                  tooltip={t("mysqlHostTooltip")}
+                >
+                  <Input
+                    placeholder={`${t("formInputPrefix")}${t("mysqlHost")}`}
+                  />
+                </Form.Item>
+                <Form.Item
+                  label={t("mysqlPort")}
+                  style={{ flex: 0.3 }}
+                  name={"port"}
+                >
+                  <InputNumber
+                    style={{ width: "100%" }}
+                    placeholder={`${t("formInputPrefix")}${t("mysqlPort")}`}
+                    min={0}
+                    max={65535}
+                    precision={0}
+                  />
+                </Form.Item>
+              </Flex>
 
+              <Flex flex={1} wrap gap={8}>
+                <Form.Item
+                  label={t("mysqlUsername")}
+                  name={"username"}
+                  style={{ flex: 1 }}
+                >
+                  <Input
+                    placeholder={`${t("formInputPrefix")}${t("mysqlUsername")}`}
+                  />
+                </Form.Item>
+                <Form.Item
+                  label={t("mysqlPassword")}
+                  name={"password"}
+                  style={{ flex: 1 }}
+                >
+                  <Input.Password
+                    placeholder={`${t("formInputPrefix")}${t("mysqlPassword")}`}
+                    iconRender={(visible) =>
+                      visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                    }
+                  />
+                </Form.Item>
+              </Flex>
               <Flex justify={"space-between"} align={"center"} flex={1}>
                 <Form.Item noStyle style={{ flex: 1 }}>
                   <Button type={"primary"} htmlType={"submit"}>
@@ -84,76 +129,81 @@ const Mysql: React.FC = () => {
                   <Checkbox onChange={onChange}>{t("mysqlRemember")}</Checkbox>
                 </Form.Item>
               </Flex>
-            </Flex>
-          </div>
-          <Divider orientation="left">{t("mysqlGenerateTitle")}</Divider>
-          <div className={"mysql-connection-panel"}>
-            <Flex justify={"space-around"} flex={1} gap={10}>
-              <Form.Item
-                label={t("mysqlSchema")}
-                style={{ flex: 1 }}
-                name={"schema"}
-              >
-                <Select />
-              </Form.Item>
-              <Form.Item
-                label={t("mysqlTable")}
-                style={{ flex: 1 }}
-                name={"table"}
-              >
-                <Select />
-              </Form.Item>
-            </Flex>
-            <Flex vertical wrap>
-              <Form.Item
-                label={t("mysqlStyle")}
-                style={{ flex: 1 }}
-                name={"style"}
-                tooltip={t("mysqlStyleTooltip")}
-              >
-                <Input
-                  placeholder={`${t("formInputPrefix")}${t("mysqlStyle")}`}
-                />
-              </Form.Item>
-              <Form.Item
-                label={t("mysqlIgnoreColumns")}
-                style={{ flex: 1 }}
-                name={"ignoreColumns"}
-              >
-                <Input
-                  placeholder={`${t("formInputPrefix")}${t("mysqlIgnoreColumns")}`}
-                />
-              </Form.Item>
-            </Flex>
-            <Flex justify={"space-around"} flex={1} gap={10}>
-              <Form.Item label={""} style={{ flex: 1 }} name={"cache"}>
-                <Checkbox>{t("mysqlCache")}</Checkbox>
-              </Form.Item>
-              <Form.Item
-                label={""}
-                style={{ flex: 1 }}
-                name={"strict"}
-                tooltip={t("mysqlStrictTooltip")}
-              >
-                <Checkbox>{t("mysqlStrict")}</Checkbox>
-              </Form.Item>
-            </Flex>
+            </div>
+            <Divider orientation="left">{t("mysqlGenerateTitle")}</Divider>
+            <div className={"mysql-connection-panel"}>
+              <Flex justify={"space-around"} flex={1} gap={10}>
+                <Form.Item
+                  label={t("mysqlSchema")}
+                  style={{ flex: 1 }}
+                  name={"schema"}
+                >
+                  <Select />
+                </Form.Item>
+                <Form.Item
+                  label={t("mysqlTable")}
+                  style={{ flex: 1 }}
+                  name={"table"}
+                >
+                  <Select />
+                </Form.Item>
+              </Flex>
+              <Flex wrap gap={8}>
+                <Form.Item
+                  label={t("mysqlStyle")}
+                  style={{ flex: 1 }}
+                  name={"style"}
+                  tooltip={t("mysqlStyleTooltip")}
+                >
+                  <Input
+                    placeholder={`${t("formInputPrefix")}${t("mysqlStyle")}`}
+                  />
+                </Form.Item>
+                <Form.Item
+                  label={t("mysqlIgnoreColumns")}
+                  style={{ flex: 1 }}
+                  name={"ignoreColumns"}
+                >
+                  <Input
+                    placeholder={`${t("formInputPrefix")}${t("mysqlIgnoreColumns")}`}
+                  />
+                </Form.Item>
+              </Flex>
+              <Flex justify={"space-around"} flex={1} gap={10}>
+                <Form.Item label={""} style={{ flex: 1 }} name={"cache"}>
+                  <Checkbox>{t("mysqlCache")}</Checkbox>
+                </Form.Item>
+                <Form.Item
+                  label={""}
+                  style={{ flex: 1 }}
+                  name={"strict"}
+                  tooltip={t("mysqlStrictTooltip")}
+                >
+                  <Checkbox>{t("mysqlStrict")}</Checkbox>
+                </Form.Item>
+              </Flex>
 
-            <Flex justify={"flex-end"}>
-              <Form.Item noStyle style={{ flex: 1 }}>
-                <Button type={"primary"} htmlType={"submit"}>
-                  {t("mysqlGenerate")}
-                </Button>
-              </Form.Item>
-            </Flex>
-          </div>
-        </Flex>
-      </Form>
-      <CodeMirror
-        style={{ flex: 0.7, height: "100%", background: "white" }}
-        className={"mysql-editor-container"}
-      />
-    </Flex>
+              <Flex justify={"flex-end"}>
+                <Form.Item noStyle style={{ flex: 1 }}>
+                  <Button type={"primary"} htmlType={"submit"}>
+                    {t("mysqlGenerate")}
+                  </Button>
+                </Form.Item>
+              </Flex>
+            </div>
+          </Flex>
+        </Form>
+        <CodeMirror
+          style={{
+            flex: 1,
+            height: "100%",
+            background: "white",
+            minWidth: 500,
+          }}
+          // className={"mysql-editor-container"}
+        />
+      </Flex>
+    </>
   );
 };
 
